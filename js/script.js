b18a05a88a2b54b8ecdc286bcf9820c2071f8aef
HenryKanyoro <kanyorohenry6@gmail.com>
2020-10-17 15:22:50 +0300

    added overlay

--- js/script.js
+++ js/script.js
@@ -1,31 +1,3 @@
-$(document).ready(function () {
-    $(".moto").click(function () {
-         $(".moto").hide(750);
-         $(".fly").show(750);
-    });
-    $(".fly").click(function () {
-         $(".fly").hide(750);
-         $(".moto").show(750);
-    });
-    $(".tamu").click(function () {
-         $(".tamu").hide(750);
-         $(".cup").show(750);
-    });
-    $(".cup").click(function () {
-         $(".cup").hide(750);
-         $(".tamu").show(750);
-    });
-    $(".plate").click(function () {
-         $(".plate").hide(750);
-         $(".kijiko").show(750);
-    });
-    $(".kijiko").click(function () {
-         $(".kijiko").hide(750);
-         $(".plate").show(750);
-    });
-});
-
-
 
 $(document).ready(function() {
      $(".photo").hover(function(){
